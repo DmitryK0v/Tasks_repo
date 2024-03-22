229a2c58901b4535ca7617a01717c52855bbc239
Koval Dmytro <97359964+DmitryK0v@users.noreply.github.com>
2024-03-22 16:13:32 +0200

    DataDog via terraform

---
+++ datadog-terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    datadog = {
+      source = "DataDog/datadog"
+    }
+  }
+}
+
+
+provider "datadog" {
+  api_url = "https://app.datadoghq.eu"
+  api_key = "**************************"
+  app_key = "**********************************"
+}
